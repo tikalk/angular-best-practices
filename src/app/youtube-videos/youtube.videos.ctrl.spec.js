4451cad4555f95ece77559bc88049645ad975233
orizens <farhioren@gmail.com>
2015-11-11 14:47:00 +0200

    added spec for youtube videos

--- src/app/youtube-videos/youtube.videos.ctrl.spec.js
+++ src/app/youtube-videos/youtube.videos.ctrl.spec.js
@@ -1,28 +1,16 @@
 describe("Youtube Videos", function() {
-	var scope, ctrl, httpBackend, url, mockData, rootScope, YoutubeSearch, YoutubePlayerSettings, YoutubeVideoInfo, $q;
+	var scope, ctrl, url, mockData, rootScope, YoutubeSearch;
 	var mockVideoItem = {};
 	var mockPlaylistItem = {};
 
 	beforeEach(module("youtube-videos"));
 
 	beforeEach(inject(
-		function($controller, $rootScope, _$q_, _YoutubeSearch_, _YoutubePlayerSettings_, _YoutubeVideoInfo_, $httpBackend){
+		function($controller, $rootScope, $injector){
 			rootScope = $rootScope;
-			$q = _$q_;
-			YoutubeSearch = _YoutubeSearch_;
-			YoutubePlayerSettings = _YoutubePlayerSettings_;
-			YoutubeVideoInfo = _YoutubeVideoInfo_;
-			httpBackend = $httpBackend;
+			YoutubeSearch = $injector.get('YoutubeSearch');
 			// spies
 			spyOn(YoutubeSearch, 'search');
-			spyOn(YoutubePlayerSettings, 'playVideoId');
-			spyOn(YoutubePlayerSettings, 'queueVideo');
-			spyOn(YoutubeVideoInfo, 'getPlaylist').and.callFake(function () {
-				var defer = $q.defer();
-				defer.resolve();
-				return defer.promise;
-			});
-			spyOn(YoutubePlayerSettings, 'playPlaylist');
 			scope = $rootScope.$new();
 			ctrl = $controller("YoutubeVideosCtrl as vm", {
 			  $scope: scope 
@@ -32,6 +20,17 @@ describe("Youtube Videos", function() {
 			mockPlaylistItem = window.mocks['youtube.videos.mock'];
 		}
 	));
+	it('should expose items props', function() {
+		expect(scope.vm.videos).toBe(YoutubeSearch.items);
+	});
+
+	it('should define set a feedType prop', function() {
+		expect(scope.vm.feedType).toBe(YoutubeSearch.getFeedType);
+	});
+
+	it('should have a loadMore function', function() {
+		expect(scope.vm.loadMore).toBe(YoutubeSearch.searchMore);
+	});
 
 	it("search youtube once, when it loads", function() {
 		expect(YoutubeSearch.search).toHaveBeenCalled();
@@ -39,8 +38,6 @@ describe("Youtube Videos", function() {
 	});
 
 	it("should queue and play video", function() {
-		scope.vm.playVideo(mockVideoItem);
-		expect(YoutubePlayerSettings.queueVideo).toHaveBeenCalled();
-		expect(YoutubePlayerSettings.playVideoId).toHaveBeenCalled();
+		expect(scope.vm.playVideo).toBeDefined();
 	});
 });
