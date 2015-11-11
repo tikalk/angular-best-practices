2c0e760551c13bf193bcbfd7a21c142a164f7b23
orizens <farhioren@gmail.com>
2015-11-11 15:58:30 +0200

    added spec for search

--- src/app/media-search/media.search.ctrl.spec.js
+++ src/app/media-search/media.search.ctrl.spec.js
@@ -1,14 +1,13 @@
 describe("Media Search", function() {
-	var scope, ctrl, httpBackend, url, mockData, rootScope, YoutubeSearch;
+	var scope, ctrl, url, mockData, rootScope, YoutubeSearch;
 	var mockVideoItem = {};
 
 	beforeEach(module("media.search"));
 
 	beforeEach(inject(
-		function($controller, $rootScope, _YoutubeSearch_, $httpBackend){
+		function($controller, $rootScope, $injector){
 			rootScope = $rootScope;
-			YoutubeSearch = _YoutubeSearch_;
-			httpBackend = $httpBackend;
+			YoutubeSearch = $injector.get('YoutubeSearch');
 			// spies
 			spyOn(YoutubeSearch, 'resetPageToken');
 			spyOn(YoutubeSearch, 'search');
@@ -22,13 +21,22 @@ describe("Media Search", function() {
 
 	it('should have a params object with q for query', function() {
 		expect(scope.vm.params).toBeDefined();
+		expect(scope.vm.params).toBe(YoutubeSearch.params);
 		expect(scope.vm.params.q).toBeDefined();
 	});
 
 	it('should have a search function', function() {
 		expect(scope.vm.search).toBeDefined();
+		expect(scope.vm.search).toBe(YoutubeSearch.search);
 	});
 
+	it('should have a resetPageToken function', function() {
+		expect(scope.vm.resetPageToken).toBe(YoutubeSearch.resetPageToken);
+	});
+
+	it('should have a complete function', function() {
+		expect(scope.vm.complete).toBeDefined();
+	});
 	it("should reset the page token when the query has changed", function() {
 		scope.vm.params.q = 'some random text ' + Date().toString();
 		scope.vm.resetPageToken();
