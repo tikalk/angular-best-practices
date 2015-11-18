86f4de8c4c69d9a25e3e0e99b7f780e9f4abf284
orizens <farhioren@gmail.com>
2015-11-18 13:13:13 +0200

    updated test to work on windows

--- gulp/test.js
+++ gulp/test.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var karma = require('karma').server;
 var isTravis = process.env.TRAVIS || false;
-var pathToKarmaConf = __dirname.replace('/gulp', '');
+var isWin = /^win/.test(process.platform);
+var pathToKarmaConf = __dirname.replace(isWin ? '\\gulp' : '/gulp', '');
 
 module.exports = gulp.task('test', function (done) {
   karma.start({
-    configFile: pathToKarmaConf + '/karma.conf.js',
+    configFile: pathToKarmaConf + (isWin ? '\\' : '') + '/karma.conf.js',
     singleRun: isTravis
   }, done);
 });
